1faadde600f40d91f7cb3af591abd71acf7df8e4
yorkeykli <yorkeykli@tencent.com>
2022-05-23 10:16:26 +0800

    :rocket: release:

--- .cz-config.js
+++ .cz-config.js
@@ -62,14 +62,14 @@ module.exports = {
     }
   ],
 
-  scopes: [{ name: 'accounts' }, { name: 'admin' }, { name: 'exampleScope' }, { name: 'changeMe' }],
+  scopes: [{ name: 'accounts' }, { name: 'admin' }, { name: '其他模块' }],
   messages: {
     type: '选择一种你的提交类型:',
     scope: '选择一个scope (可选):',
     // used if allowCustomScopes is true
-    customScope: '此次更改范围::',
+    customScope: '此次更改范围:',
     subject: '短说明:\n',
-    body: '长说明，使用"|"换行(可选)：\n',
+    body: '长说明，使用"|"换行(可选):\n',
     breaking: '非兼容性说明 (可选):\n',
     footer: '关联关闭的issue，例如：#31, #34(可选):\n',
     confirmCommit: '确定提交说明? yes/no'
@@ -80,8 +80,8 @@ module.exports = {
   skipQuestions: ['body'],
 
   // limit subject length
-  subjectLimit: 100
-  // breaklineChar: '|', // It is supported for fields body and footer.
-  // footerPrefix : 'ISSUES CLOSED:'
+  subjectLimit: 100,
+  breaklineChar: '|', // It is supported for fields body and footer.
+  footerPrefix: 'ISSUES CLOSED:'
   // askForBreakingChangeFirst : true, // default is false
 };

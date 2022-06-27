170345a36a77164efa2a3fea0ecc029ed7a82666
yorkeykli <yorkeykli@tencent.com>
2022-06-27 19:02:50 +0800

    [refactor](其他模块): uI组件库 TDesign 改为按需引入

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,8 @@ import App from './App.vue';
 import router from './router';
 import piniaStore from './store';
 import './index.css';
-//改为TDesign默认UI
-import TDesign from 'tdesign-vue-next';
-// 引入组件库全局样式资源
-import 'tdesign-vue-next/es/style/index.css';
 import './permission';
 
 // 支持SVG vite-plugin-svg-icons
 import 'virtual:svg-icons-register';
-createApp(App).use(router).use(TDesign).use(piniaStore).mount('#app');
+createApp(App).use(router).use(piniaStore).mount('#app');
